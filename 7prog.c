d2f763affea49a6b4ecdc3ad1ef00a98d317b82a
Naahin <98544401+Naahin@users.noreply.github.com>
2022-03-28 22:12:21 +0600

    Add files via upload

---
+++ 7prog.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,c;
+    printf("welcome to division program");
+    printf("\nEnter value for a: ");
+    scanf("%f",&a);
+    printf("\nEnter value for b: ");
+    scanf("%f",&b);
+    c=a*b;
+    printf("devision result: %f",c);
+
+
+}
+
